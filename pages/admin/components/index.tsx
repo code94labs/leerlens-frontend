0a23f20263f236120620519bb1c1a84fb2bfd495
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2024-04-11 13:16:20 +0530

    updates

---
+++ pages/admin/components/index.tsx
@@ -0,0 +1,21 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+import Sidebar from "../../../shared/Sidebar/Sidebar";
+import AdminHeader from "../../../shared/Header/AdminHeader";
+import DynamicDropdown from "../../../shared/DynamicDropdown";
+
+const ComponentsPage = () => {
+  return (
+    <Stack direction="row">
+      <Sidebar />
+
+      <Stack width={"100%"}>
+        <AdminHeader title="Component Test" />
+
+        <DynamicDropdown />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default ComponentsPage;
