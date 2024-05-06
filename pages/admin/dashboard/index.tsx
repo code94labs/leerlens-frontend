7f581efec99a8088bd92e5e9f23eeb0d7bf73936
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2024-05-06 09:26:42 +0530

    test pages for charts

--- pages/admin/dashboard/index.tsx
+++ pages/admin/dashboard/index.tsx
@@ -1,42 +1,31 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, ButtonGroup, Stack, Typography } from "@mui/material";
 import React from "react";
+import { useRouter } from "next/navigation";
 import Sidebar from "../../../shared/Sidebar/Sidebar";
 import AdminHeader from "../../../shared/Header/AdminHeader";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  resetForm,
-  selectForm,
-  setFormModified,
-} from "../../../redux/slices/formSlice";
 
 const DashboardPage = () => {
-  const dispatch = useDispatch();
-
-  const formDetails = useSelector(selectForm);
-
-  const onHandleUpdate = () => {
-    dispatch(setFormModified());
-  };
-
-  const onHandleResetForm = () => {
-    dispatch(resetForm());
-  };
+  const router = useRouter();
 
   const testComponent = (
     <>
-      <Stack flexDirection="row" justifyContent="space-evenly" mt={5}>
-        <Button onClick={onHandleUpdate} variant="contained">
-          Trigger the Form update state redux
-        </Button>
-
-        <Button onClick={onHandleResetForm} variant="contained">
-          Reset the form details update state
-        </Button>
-      </Stack>
-
       <Typography variant="h4" textAlign="center" mt={5}>
-        {formDetails.isModified ? "Form is modified" : "form is not modified"}
+        Chart types and variations
       </Typography>
+
+      <ButtonGroup
+        variant="contained"
+        aria-label="Basic button group"
+        sx={{ marginTop: 5, marginX: 5 }}
+      >
+        <Button
+          onClick={() => router.push("./dashboard/vertical-bar-chart-01")}
+        >
+          Vertical bar charts - Type 01
+        </Button>
+        <Button>Vertical bar charts - Type 02</Button>
+        <Button>Line charts</Button>
+      </ButtonGroup>
     </>
   );
 
@@ -47,7 +36,7 @@ const DashboardPage = () => {
       <Stack width={"100%"}>
         <AdminHeader title="Dashboard" shouldDisplayBreadcrumb />
 
-        {/* {testComponent} */}
+        {testComponent}
         {/* Content */}
       </Stack>
     </Stack>
