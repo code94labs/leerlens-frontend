50eebe48c1e1e6138c01e5b8f68a02764fa7c878
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2024-05-30 10:04:57 +0530

    basic tabs for sidebar

--- routes/routes.ts
+++ routes/routes.ts
@@ -15,6 +15,24 @@ export const pages: RoutePath[] = [
     text: "Learning Scan Report",
     path: "/admin/dashboard",
     iconPath: "/images/admin-icons/reports.png",
+    sub: [
+      {
+        text: "NormGroup",
+        path: "/admin/dashboard/norm-group",
+      },
+      {
+        text: "Evaluation",
+        path: "/admin/dashboard/evaluation",
+      },
+      {
+        text: "Samengevat",
+        path: "/admin/dashboard/samengevat",
+      },
+      {
+        text: "EML",
+        path: "/admin/dashboard/eml",
+      },
+    ],
   },
   {
     text: "Responses",
@@ -49,7 +67,6 @@ export const pages: RoutePath[] = [
     iconPath: "/images/admin-icons/settings.png",
   },
 ];
-
 
 export const breadcrumbMap: { [key: string]: string } = {
   "/admin/dashboard": "Learning Scan Report",
