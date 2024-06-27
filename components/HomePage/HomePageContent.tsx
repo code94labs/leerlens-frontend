fee783291d8a2ef5bee7f2c6438346ffa9a69404
omin <wathsaradesilva2000@gmail.com>
2024-06-27 14:21:22 +0530

    feat(Intial i18next package set up for text extraction and translation): English translations for Home page

--- components/HomePage/HomePageContent.tsx
+++ components/HomePage/HomePageContent.tsx
@@ -30,27 +30,27 @@ const HomePageContent = () => {
       <Stack flexDirection="row" sx={customStyles.innerStack}>
         <FormCard
           title={t('home.pre-intervention.title')}
-          description="Great that you are participating in the learning scan! Completing the learning scan takes approximately five minutes. Answer the questions as honestly as possible: there is no right or wrong. With all the answers we collect we can investigate what you think about learning. Goodluck!"
+          description={t('home.pre-intervention.description')}
           pagePath="/forms/pre-intervention"
         />
 
         <FormCard
           title={t('home.post-intervention.title')}
-          description="Great that you are participating in the learning scan! Completing the learning scan takes approximately five minutes. Answer the questions as honestly as possible: there is no right or wrong. With all the answers we collect we can investigate what you think about learning. Goodluck!"
+          description={t('home.pre-intervention.description')}
           pagePath="/forms/post-intervention"
         />
       </Stack>
 
       <Stack flexDirection="row" sx={customStyles.innerStack}>
         <FormCard
-          title="Remind Evaluation"
-          description="How cool that you did one or more training sessions with us. We have done our best to make it as fun and educational as possible for you and are very curious about what you thought of it. That's why we have a few questions for you. Answer them honestly, whether you are positive or negative about the training, we can learn from it! :)"
+          title={t('home.Remind Evaluation.title')}
+          description={t('home.Remind Evaluation.description')}
           pagePath="/forms/remind-evaluation"
         />
 
         <FormCard
-          title="Teachers"
-          description="Great that you are participating in the learning scan! Completing the learning scan takes approximately five minutes. Answer the questions as honestly as possible: there is no right or wrong. With all the answers we collect we can investigate what you think about learning. Goodluck!"
+          title={t('home.Teachers.title')}
+          description={t('home.Teachers.description')}
           pagePath="/teachers"
         />
       </Stack>
